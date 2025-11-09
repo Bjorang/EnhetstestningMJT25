0104842b0fd772e53be2c614a82efc719dcfc9e5
tomans <tom.anstensen@elevera.org>
2025-11-09 20:28:36 +0100

    Initializing Maven project for testers

---
+++ src/main/java/iths/EnhetstestningMJT25.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package iths;
+
+/**
+ *
+ * @author tomans
+ */
+public class EnhetstestningMJT25 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
